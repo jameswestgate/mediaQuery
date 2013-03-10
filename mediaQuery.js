4f5ad5616f0fecbb905e8b988fdb7b36b1b98611
James Westgate <jamesw@opencomponents.com>
2013-03-10 15:23:41 +0000

    Update mediaQuery.js

--- mediaQuery.js
+++ mediaQuery.js
@@ -1,31 +1,38 @@
-  	//Set up media query classes
+(function($){
+		
+	$.fn.mediaQuery = function(options){
+				
 		if (window.matchMedia) {
-			
-			var queries = {
-				w320: 'only screen and (min-width: 320px)',
-				w480: 'only screen and (min-width: 480px) ',
-				w768: 'only screen and (min-width: 768px)',
-				w1024: 'only screen and (min-width: 1024px)'
-			}
 
 			var keys = '';
-
+			var queries = options.queries;
+			var $this = $(this);
+				
 			for (var key in queries) {
-
-				var mq = window.matchMedia(queries[key]);
-				mq.addListener(mediaChange);
-				//mediaChange();
-
+				
 				keys += key + ' ';
-
-				(function(key) {
-					function mediaChange(mql) {
-						console.log('removing ' + keys)
-						$('body').removeClass(keys);
-
-						console.log('adding ' + key)
-						$('body').addClass(key);
-					}
-				})(key);
+				var mq = window.matchMedia(queries[key]);
+				
+				if (mq.addListener) {
+					(function(key, keys, mq) {
+						mq.addListener(function() {
+							$this .removeClass(keys);
+							$this .addClass(key);
+						});
+					})(key, keys, mq);
+				}	
 			}
-		}
+				
+			return this;
+		};
+	}	
+})(jQuery);
+
+
+// {queries : {
+// 	w320: 'only screen and (min-width: 320px)',
+// 	w480: 'only screen and (min-width: 480px) ',
+// 	w768: 'only screen and (min-width: 768px)',
+// 	w1024: 'only screen and (min-width: 1024px)'
+// }
+		
