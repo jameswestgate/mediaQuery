9871264b66b9a34c26c86c33d9aa68d5f5df4932
James Westgate <jamesw@opencomponents.com>
2013-03-18 17:04:39 +0000

    Update mediaQuery.js

--- mediaQuery.js
+++ mediaQuery.js
@@ -1,38 +1,32 @@
+
+/* A jQuery plugin that uses media queries to add classes to any element */
+/* 
+/* Copyright James Westgate 2013 */
+/* Dual licensed under the MIT and GPL licenses */
+
 (function($){
 		
 	$.fn.mediaQuery = function(options){
-				
-		if (window.matchMedia) {
+		
+		//Feature detect
+		if (!window.matchMedia) return;
+		if (!window.matchMedia('only screen').addListener) return;
 
-			var keys = '';
-			var queries = options.queries;
-			var $this = $(this);
-				
-			for (var key in queries) {
-				
-				keys += key + ' ';
-				var mq = window.matchMedia(queries[key]);
-				
-				if (mq.addListener) {
-					(function(key, keys, mq) {
-						mq.addListener(function() {
-							$this .removeClass(keys);
-							$this .addClass(key);
-						});
-					})(key, keys, mq);
-				}	
-			}
-				
-			return this;
-		};
+		var queries = options.queries;
+		var $this = $(this);
+			
+		for (var key in queries) {
+			
+			var mq = window.matchMedia(queries[key]);
+			
+			(function(key) {
+				mq.addListener(function(mql) {
+					(mql.matches) ? $this.addClass(key) : $this.removeClass(key);
+				});
+				(mq.matches) ? $this.addClass(key) : $this.removeClass(key);
+			})(key);
+		}	
+		
+		return this;
 	}	
 })(jQuery);
-
-
-// {queries : {
-// 	w320: 'only screen and (min-width: 320px)',
-// 	w480: 'only screen and (min-width: 480px) ',
-// 	w768: 'only screen and (min-width: 768px)',
-// 	w1024: 'only screen and (min-width: 1024px)'
-// }
-		
